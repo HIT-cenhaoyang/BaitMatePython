365cbdd117ee249ad43d044c8f226572702ff0b8
MaxC <31586456+HIT-cenhaoyang@users.noreply.github.com>
2025-01-30 16:25:04 +0800

    Changed to Post Method to upload images to predict results Changed  result to include multiple results and possibilities

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import io
 
 import psycopg2
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
 
 from keras.src.layers import BatchNormalization
 from keras.models import load_model
@@ -18,33 +18,6 @@ def resize_image(image, target_size=(224, 224)):
         image = np.stack((image,) * 3, axis=-1)
     return image
 
-def get_image_from_db(image_id):
-    try:
-        conn = psycopg2.connect(
-            dbname="postgres",
-            host="localhost",
-            user="postgres",
-            password="123456"
-        )
-        cur = conn.cursor()
-        cur.execute("SELECT fish_image FROM baitmate.fish WHERE id = %s", (image_id,))
-        oid = cur.fetchone()
-        lobj_manager = conn.lobject(oid=oid[0], mode="rb")
-        # Read image data
-        image_data = lobj_manager.read()
-        lobj_manager.close()
-        # Convert binary to image
-        image = Image.open(io.BytesIO(image_data))
-        cur.close()
-        conn.close()
-        if image_data is None:
-            abort(404, description="Image not found")
-        return image
-    except psycopg2.OperationalError as e:
-        print(f"Database connection error: {e}")
-        abort(500, description="Database connection error")
-
-
 def predict(img):
     model = load_model('./static/FishModelClassifier_V6.h5', compile=False, custom_objects={'BatchNormalization': BatchNormalization})
     class_name = ['Bangus', 'Big Head Carp', 'Black Spotted Barb', 'Catfish', 'Climbing Perch', 'Fourfinger Threadfin',
@@ -60,21 +33,20 @@ def predict(img):
     img = np.expand_dims(img, [0])
     answer = model.predict(img)
     x = list(np.argsort(answer[0])[::-1][:5])
+    results = []
 
     for i in x:
+        results.append((class_name[i], float(answer[0][i]) * 100))
         print("{className}: {predVal:.2f}%".format(className=class_name[i], predVal=float(answer[0][i]) * 100))
 
-    y_class = answer.argmax(axis=-1)
-    # y = " ".join(str(x) for x in y_class)
-    y = int(y_class[0])
-    res = class_name[y]
+    return results
 
-    return res
-
-@app.route('/image/<string:image_id>', methods=['GET'])
-def predict_fish(image_id):
-    image_data = get_image_from_db(image_id)
-    result = predict(image_data)
+@app.route('/image/predict', methods=['POST'])
+def predict_fish():
+    file = request.files['image']
+    image_data = file.read()
+    image = Image.open(io.BytesIO(image_data))
+    result = predict(image)
     return jsonify(result)
 
 if __name__ == '__main__':
